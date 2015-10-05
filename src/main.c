13234c45063216257d97a8c7f1f56a3ddcd64410
brunoChr <b.christol@net.estia.fr>
2015-10-05 10:55:15 +0100

    /*** ADD FILES ***/ - project files - serial data

--- src/main.c
+++ src/main.c
@@ -44,17 +44,20 @@ int main(void)
 	BYTE randData;
 	BYTE *thermalData;
 	BYTE index;
-	uint16_t adcResult;
+	uint16_t adcResultCh0, adcResultCh1;
 	int8_t  distanceIRrLeft, distanceIrRight;
 	
 	
 	/*** VARIABLE INITIALISATION ***/
 	randData = 0;
 	index = 0;
-	adcResult = 0;
+	adcResultCh0 = 0;
+	adcResultCh1 = 0;
 	distanceIrRight = 0;
 	distanceIRrLeft = 0;
 	
+	_delay_ms(1000);
+		
 	/*** SETUP SYSTEM ***/
 	setup();
 	
@@ -69,33 +72,34 @@ int main(void)
 	{	
 		
 		/*** TEST THERMAL SENSOR ***/		
-		thermal_read(0x14, thermalData);
+		//thermal_read(0x14, thermalData);
 		//mesure_thermal(thermal_data, THERMAL_BUFF_SIZE-1);
 	
 		//printf("\n\rthermalData : ");
 	
-		for (index = 0; index < THERMAL_BUFF_SIZE; index++ )
+		/*for (index = 0; index < THERMAL_BUFF_SIZE; index++ )
 		{
 			uart_putchar(thermalData[index]);
-		}
+		}*/
 				
 		/*** TEST ADC CHANNEL 0 ***/
-		adcResult = adc_read(ADC_CH_IR_RIGHT);
+		adcResultCh0 = adc_read(ADC_CH_IR_RIGHT);
 		
 		/*** TEST IR SENSOR ***/
-		distanceIrRight = sharp_IR_interpret_GP2Y0A02YK(adcResult);
+		distanceIrRight = sharp_IR_interpret_GP2Y0A02YK(adcResultCh0);
 		
 		
 		/*** TEST ADC CHANNEL 1 ***/
-		//adcResult = 0;
-		adcResult = adc_read(ADC_CH_IR_LEFT);
+		adcResultCh1 = adc_read(ADC_CH_IR_LEFT);
 				
 		/*** TEST IR SENSOR ***/
-		distanceIRrLeft = sharp_IR_interpret_GP2Y0A02YK(adcResult);
+		distanceIRrLeft = sharp_IR_interpret_GP2Y0A02YK(adcResultCh1);
 		
-		/* PRINT */
-		uart_putchar(distanceIrRight);
-		uart_putchar(distanceIRrLeft);
+		/* PRINT ADC VALUES */
+		printf("%d\t%d\t%d\t%d\r",adcResultCh0, adcResultCh1, distanceIrRight, distanceIRrLeft);
+		//printf("%d\t%d\r",distanceIrRight, distanceIRrLeft);
+		//uart_putchar(distanceIrRight);
+		//uart_putchar(distanceIRrLeft);
 		
 		_delay_ms(1000);	
  		}
---
+++ src/serialData.c
@@ -0,0 +1,14 @@
+/*
+ * serialData.c
+ *
+ * Created: 05/10/2015 10:34:25
+ *  Author: b.christol
+ */ 
+
+
+BOOL formatProtocol(BYTE *data)
+{
+	
+	
+	
+}
