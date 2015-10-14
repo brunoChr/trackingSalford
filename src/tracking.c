5546a9f5ec5b04aa9a675cad5444dec4afc300bd
a.moufounda <arthur.moufounda@gmail.com>
2015-10-14 17:22:41 +0100

    // Modif fonction Tracking

--- src/tracking.c
+++ src/tracking.c
@@ -6,6 +6,8 @@
  */ 
 
 #include "../lib/pwm.h"
+#include "../lib/types.h"
+#include "../lib/tracking.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,8 +15,8 @@
 #define NEED_ROTATION_GAUCHE 1
 #define NEED_ROTATION_DROITE 2
 #define CIBLE_TRACKEE 3
-#define DISTANCE_MAX 80
-#define DISTANCE_MIN 20
+#define DISTANCE_MAX 1500
+#define DISTANCE_MIN 200
 
 /*! \fn int info_tracking(int8_t distanceIrRight, int8_t distanceIrLeft)
  *  \brief inform about the direction the pwm need to follow
@@ -24,7 +26,7 @@
  *  \return direction
  */
 
-int info_tracking(int8_t distanceIrRight, int8_t distanceIrLeft)
+int info_tracking(UINT distanceIrRight, UINT distanceIrLeft)
 {
 	//!< if the value from both right and left sensors are out of the range (distance > 150 cm)
 	if ((distanceIrRight >= DISTANCE_MAX) && (distanceIrLeft >= DISTANCE_MAX))
@@ -37,17 +39,20 @@ int info_tracking(int8_t distanceIrRight, int8_t distanceIrLeft)
 		return OUT_OF_RANGE;
 	}
 	//!< if someone is in front of the left sensor but not in front of the right one, the indication must be to turn on left
-	else if((distanceIrRight >= DISTANCE_MAX) && (distanceIrLeft < DISTANCE_MAX))
+	else if(((distanceIrRight >= DISTANCE_MAX) || (distanceIrRight < DISTANCE_MIN)) &&
+	 ((distanceIrLeft < DISTANCE_MAX) && (distanceIrLeft >= DISTANCE_MIN)))
 	{
 		return NEED_ROTATION_GAUCHE;
 	}
 	//!< if someone is in front of the right sensor but not in front of the left one, the indication must be to turn on right
-	else if((distanceIrRight < DISTANCE_MAX) && (distanceIrLeft >= DISTANCE_MAX))
+	else if(((distanceIrLeft>= DISTANCE_MAX) || (distanceIrLeft < DISTANCE_MIN)) &&
+	((distanceIrRight < DISTANCE_MAX) && (distanceIrRight >= DISTANCE_MIN)))
 	{
 		return NEED_ROTATION_DROITE;
 	}
 	//!< if someone is in front of the both sensors, the target is well-focused
-	else if((distanceIrRight < DISTANCE_MAX) && (distanceIrLeft < DISTANCE_MAX))
+	else if(((distanceIrRight < DISTANCE_MAX) && (distanceIrRight >= DISTANCE_MIN))
+	 && ((distanceIrLeft < DISTANCE_MAX) && (distanceIrLeft >= DISTANCE_MIN)))
 	{
 		return CIBLE_TRACKEE;
 	}
@@ -55,39 +60,43 @@ int info_tracking(int8_t distanceIrRight, int8_t distanceIrLeft)
 	return 0;
 }
 
-/*! \fn void tracking(int8_t distanceIrRight, int8_t distanceIrLeft)
- *  \brief follow the target according to the information given by the function "info_tracking"
- *  \param distanceIrRight : distance read by the right infrared sensor
- *  \param distanceIrLeft : distance read by the left infrared sensor
- *  \exception 
- *  \return 
- */
-void tracking(int8_t distanceIrRight, int8_t distanceIrLeft)
+unsigned int tracking(UINT distanceIrRight, UINT distanceIrLeft, int position)
 {
-	int i = 0;
-	while ((info_tracking(distanceIrRight, distanceIrLeft) != CIBLE_TRACKEE)
-			&& (info_tracking(distanceIrRight, distanceIrLeft) != OUT_OF_RANGE))
+	/*! \fn unsigned int tracking(UINT distanceIrRight, UINT distanceIrLeft, int position)
+	*	\brief follow the target according to the information given by the function "info_tracking"
+	*	\param distanceIrRight : distance read by the right infrared sensor
+	*	\param distanceIrLeft : distance read by the left infrared sensor
+	*	\param position : actual position of the servomotor, in degrees
+	*	\exception
+	*	\return the new position of the servo, in degrees
+	*/
+	switch(info_tracking(distanceIrRight, distanceIrLeft))
 	{
-		if(info_tracking(distanceIrRight, distanceIrLeft) == NEED_ROTATION_DROITE)
-		{
-			if(i < 0)
-			//!< if the pwm previously went to the left, 'i' must be reinitialized
-			{
-				i = 0;
-			}
-			pwm_setPosition(91 + i);
-			i++;
-		}
 		
-		else if(info_tracking(distanceIrRight, distanceIrLeft) == NEED_ROTATION_GAUCHE)
-		{
-			//!< if the pwm previously went to the right, 'i' must be reinitialized
-			if(i > 0)
-			{
-				i = 0;
-			}
-			pwm_setPosition(89 + i);
-			i--;
-		}
+		case OUT_OF_RANGE:
+			pwm_positionCentrale();
+			return 90;
+			break;
+		
+		case NEED_ROTATION_DROITE:
+			position++;
+			pwm_setPosition(position);
+			return position;
+			break;
+			
+		case NEED_ROTATION_GAUCHE:
+			position--;
+			pwm_setPosition(position);
+			return position;
+			break;
+		
+		case CIBLE_TRACKEE:
+			pwm_setPosition(position);
+			return position;
+			
+		default:
+			return position;
+			break;
 	}
+	return position;				
 }
